a9dbf8dfb3f0fe304a918199d7d2c9d7a0142a40
Jasper De Moor <jasperdemoor@gmail.com>
2019-04-09 14:03:37 +0200

    add anotation to day-3

--- day-3/index.js
+++ day-3/index.js
@@ -1,3 +1,7 @@
+// In JavaScript it's probably easier to just JSON.stringify and JSON.parse this stuff...
+// But the question was originally a python question where this is kinda not such a core thing.
+// Not really happy about my solution though... Was good practise though.
+
 const assert = require("assert");
 
 // Node Class
