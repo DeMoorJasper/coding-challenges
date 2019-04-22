b4236b1ab2fbe40f9ef612e272153f747cf10141
Jasper De Moor <jasperdemoor@gmail.com>
2019-04-22 16:30:52 +0200

    solve some more i guess

---
+++ day-7/index.js
@@ -0,0 +1,5 @@
+// I kinda don't understand the question so won't be solving this one...
+
+let mapping = {
+  // the alphabet
+};
---
+++ day-8/index.js
@@ -0,0 +1,33 @@
+let exampleTree = {
+  value: 0,
+  leftChild: {
+    value: 1,
+    leftChild: null,
+    rightChild: null
+  },
+  rightChild: {
+    value: 0,
+    leftChild: {
+      value: 1,
+      leftChild: {
+        value: 1,
+        leftChild: null,
+        rightChild: null
+      },
+      rightChild: {
+        value: 1,
+        leftChild: null,
+        rightChild: null
+      }
+    },
+    rightChild: {
+      value: 0,
+      leftChild: null,
+      rightChild: null
+    }
+  }
+};
+
+// TODO: Do some research on unival tree and what it actually is
+// Can't seem to figure out that the answer is 5
+
